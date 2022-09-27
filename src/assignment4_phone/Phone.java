db67f10c21f14848ed63509eb529dbc39dc2127e
Administrator <tranvanthanh060699@gmail.com>
2022-09-27 18:34:31 +0700

    Up code

--- src/assignment4_phone/Phone.java
+++ src/assignment4_phone/Phone.java
@@ -4,10 +4,10 @@ public abstract class Phone {
     public String name;
     public String phone;
 
-    public abstract void insertPhone ();
-    public abstract void removePhone();
-    public abstract void updatePhone();
-    public abstract void searchPhone();
+    public abstract void insertPhone(String name, String phone);
+    public  abstract void removePhone(String name);
+    public abstract void updatePhone(String name,String oldPhone, String newPhone);
+    public abstract PhoneNumber searchPhone(String name);
     public abstract void sort();
 
 }
--- src/assignment4_phone/PhoneBook.java
+++ src/assignment4_phone/PhoneBook.java
@@ -4,27 +4,56 @@ import java.util.ArrayList;
 
 public class PhoneBook extends Phone{
     public ArrayList<PhoneNumber> PhoneList;
+    public PhoneBook(){
+        PhoneList = new ArrayList<>();
+    }
     @Override
-    public void insertPhone() {
-        for (PhoneNumber pl: PhoneList){
-            if (pl.name != pl.name)
+    public void insertPhone(String name, String phone) {
+        for (PhoneNumber p :PhoneList) {
+            if (p.getName().equals(name)){
+                if (p.getPhone().contains(phone)){
+                    System.out.println("Liên lạc đã có");
+                    return;
+                }
+                // có tên nhưngg chưa có số
+                p.getPhone().add(phone);
+                System.out.println("Đã thêm"+phone+"vào liên lạc có sẵn");
+                return;
+            }
+        }
+        // nếu mà k có
+        PhoneNumber pn = new PhoneNumber(name,phone);
+        PhoneList.add(pn);
+        System.out.println("Đã thêm"+name+"vào liên lạc");
+    }
 
+    @Override
+    public void removePhone(String name) {
+        for (PhoneNumber p:PhoneList) {
+            if (p.getName().equals(name)){
+                PhoneList.remove(p);
+            }
         }
     }
 
     @Override
-    public void removePhone() {
-
+    public void updatePhone(String name, String oldPhone, String newPhone) {
+        for (PhoneNumber p:PhoneList) {
+            if (p.getName().equals(name)){
+                p.getPhone().remove(oldPhone);
+                p.getPhone().add(newPhone);
+            }
+        }
     }
 
     @Override
-    public void updatePhone() {
-
-    }
-
-    @Override
-    public void searchPhone() {
-
+    public PhoneNumber searchPhone(String name) {
+        for (PhoneNumber p:PhoneList) {
+            if (p.getName().equals(name)){
+                return p;
+            }
+        }
+        return null;
     }
 
     @Override
