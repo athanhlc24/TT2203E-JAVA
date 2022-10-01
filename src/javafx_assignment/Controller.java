9e6a4a35b9897fd677d03bcae3c5940656e8a5a2
Administrator <tranvanthanh060699@gmail.com>
2022-10-01 19:30:10 +0700

    Up code

--- src/javafx_assignment/Controller.java
+++ src/javafx_assignment/Controller.java
@@ -1,5 +1,29 @@
 package javafx_assignment;
 
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+    public TextField txtFullName;
+    public TextField txtEmail;
+    public TextField txtAge;
+    public TextField txtAddress;
+    public Text tFullName;
+    public Text tEmail;
+    public Text tAge;
+    public Text tAddress;
+    public void handleSubmit(){
+        String fn = txtFullName.getText();
+        tFullName.setText(fn);
+
+        String em = txtEmail.getText();
+        tEmail.setText(em);
+
+        String ag = txtAge.getText();
+        tAge.setText(ag);
+
+        String ad = txtAddress.getText();
+        tAddress.setText(ad);
+    }
 
 }
