b486b4c7aa08f986ece94e96c0607d070456e1e5
Administrator <tranvanthanh060699@gmail.com>
2022-10-04 18:04:06 +0700

    Up code

--- src/student_assignment/Controller.java
+++ src/student_assignment/Controller.java
@@ -1,6 +1,5 @@
 package student_assignment;
 
-import fix_assignment_danh_ba_dien_thoai.PhoneNumbers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
@@ -13,34 +12,25 @@ public class Controller {
     public TextField txtMark;
     public Text error;
     public ListView<Students> lvStudent;
-    private ObservableList<PhoneNumbers> phoneList = FXCollections.observableArrayList();
+    private ObservableList<Students> studentList = FXCollections.observableArrayList();
 
     public void studentSubmit(){
         try {
             error.setVisible(false);
             if(txtName.getText().isEmpty() || txtEmail.getText().isEmpty()||txtMark.getText().isEmpty()){
-                throw new Exception("Vui lòng nhập đầy đủ tên và số điện thoại");
+                throw new Exception("Vui lòng nhập đầy đủ tên, email và điểm");
             }
 //
             printResult();
         }catch (Exception e){
-            errors.setText(e.getMessage());
-            errors.setVisible(true);
+            error.setText(e.getMessage());
+            error.setVisible(true);
         }
     }
 
-    public void updatePhone(){
-        for(PhoneNumbers p:phoneList){
-            if(p.getName().equals(txtName.getText())){
-                p.setPhone(txtPhone.getText());
-                return;
-            }
-        }
-        phoneList.add(new PhoneNumbers(txtName.getText(),txtPhone.getText()));
-    }
 
     public void printResult(){
-        lv.setItems(phoneList);
-        lv.refresh();
+        lvStudent.setItems(studentList);
+        lvStudent.refresh();
     }
 }
